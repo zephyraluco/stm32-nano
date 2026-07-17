fed82c7609140dd7791be16f7a04198d7fd331e8
zephyraluco <zephyraluco@gmail.com>
2026-07-18 00:25:55 +0800

    first commit

---
+++ Middlewares/Third_Party/RealThread_RTOS_RT-Thread/components/finsh/inc/finsh_config.h
@@ -0,0 +1,25 @@
+/* FinSH config file */
+
+#ifndef __MSH_CFG_H__
+#define __MSH_CFG_H__
+
+// <<< Use Configuration Wizard in Context Menu >>>
+#define FINSH_USING_MSH
+#define FINSH_USING_MSH_ONLY
+// <h>FinSH Configuration
+// <o>the priority of finsh thread <1-30>
+//  <i>the priority of finsh thread
+//  <i>Default: 21
+#define FINSH_THREAD_PRIORITY       21
+
+#define FINSH_USING_SYMTAB
+// <c1>Enable command description
+//  <i>Enable command description
+#define FINSH_USING_DESCRIPTION
+//  </c>
+// </h>
+#define MSH_USING_BUILT_IN_COMMANDS
+
+// <<< end of configuration section >>>
+#endif
+
