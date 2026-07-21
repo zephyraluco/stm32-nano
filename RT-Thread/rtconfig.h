e6e0848a857e2fcb43f03c572aa1f1c2223259ac
zephyraluco <zephyraluco@gmail.com>
2026-07-22 01:34:22 +0800

    feat(samples): add event and mailbox RT-Thread samples
    - Add event_sample.c demonstrating RT-Thread event OR/AND receive
    - Add mailbox_sample.c demonstrating mailbox send/recv between threads
    - Enable RT_USING_EVENT and RT_USING_MAILBOX in rtconfig.h
    - Register new sources in cmake/stm32cubemx/CMakeLists.txt
    - Reduce RT_HEAP_SIZE from 15KB to 10KB in board.c

--- RT-Thread/rtconfig.h
+++ RT-Thread/rtconfig.h
@@ -104,12 +104,12 @@
 
 // <c1>Using Event
 //  <i>Using Event
-//#define RT_USING_EVENT
+#define RT_USING_EVENT
 // </c>
 
 // <c1>Using MailBox
 //  <i>Using MailBox
-//#define RT_USING_MAILBOX
+#define RT_USING_MAILBOX
 // </c>
 
 // <c1>Using Message Queue
